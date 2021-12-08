45595a0f840658fc27ace4a82ee3f88d8a2dbf31
Nadiv <nadivpanitch@gmail.com>
2021-12-07 21:01:04 -0500

    yo

--- Intersection.py
+++ Intersection.py
@@ -1,3 +1,14 @@
+import bluetooth as comm
+
+STRAIGHT =  1
+LEFT = 2
+RIGHT = 3
+UTURN = 4
+
+NORTH = 1
+EAST = 2
+WEST = 4
+SOUTH = 3
 
 
 class Intersection(object):
@@ -38,13 +49,15 @@ class Intersection(object):
         self.finished = True
 
 class Mouse(object):
-    mouse_number
-    curr_node
+    mouse_number = 0
+    orientation = NORTH
+    curr_node = None
     prev_node = None
 
     def __init__(self, mouse, root):
         self.mouse_number = mouse
         self.curr_node = root
+        self.orientation = NORTH
     
     def update_node(self, node):
         self.prev_node = self.curr_node
@@ -52,27 +65,44 @@ class Mouse(object):
     
     def turnLeft(self):
         if self.curr_node.prev == self.prev_node:
-            #turn Left
-            return
+            data = comm.send_instr(LEFT)
+            return data
 
     def turnStraight(self):
         if self.curr_node.prev == self.prev_node:
-            #goStraight
-            return
+            data = comm.send_instr(STRAIGHT)
+            return data
         elif self.prev_node == self.curr_node.left:
-            #turnLeft
-            return
+            data = comm.send_instr(LEFT)
+            return data
     
     def turnRight(self):
         if self.curr_node.prev == self.prev_node:
-            #turn right
-            return
+            data = comm.send_instr(RIGHT)
+            return data
         elif self.prev_node == self.curr_node.left:
-            #go straight
-            return
+            data = comm.send_instr(STRAIGHT)
+            return data
+            
         elif self.prev_node == self.curr_node.straight:
-            #turn left
-            return
+            data = comm.send_instr(LEFT)
+            return data
+
+def updateLocation(location, mouse, dist, turnDir):
+    if turnDir == LEFT:
+        mouse.orientation = (mouse.orientation - 1)%4
+    elif turnDir == RIGHT:
+        mouse.orientation = (mouse.orientation + 1)%4
+    if mouse.orientation == NORTH:
+        location[0] += dist
+    if mouse.orientation == EAST:
+        location[1] += dist
+    if mouse.orientation == WEST:
+        location[1] -= dist
+    if mouse.orientation == SOUTH:
+        location[0] -= dist
+    return location
+
 
 
 
@@ -103,47 +133,104 @@ def return_to_alive(node, mouse):
     else:
         if node == node.prev.left:
             #turn right and drive to next node
+            comm.send_instr(RIGHT)
             mouse.update_node(node.prev)
             return_to_alive(node.prev, mouse)
         elif node == node.prev.straight:
             #drive straight to next node
+            comm.send_instr(STRAIGHT)
             mouse.update_node(node.prev)
             return_to_alive(node.prev, mouse)
         else:
             #turn left and drive to next node
+            comm.send(LEFT)
             mouse.update_node(node.prev)
             return_to_alive(node.prev, mouse)
 
-def check_children(curr):
+def check_children(curr, mouse):
+    
     if curr.turns(1)==1 & curr.left.dead == False:
-        #turn_left(mouse_number)
-        #drive forward to next node(mouse_num)
-        #wait for mouse to idle
+        
+        #turn_left and drive to next node(mouse_number)
+        [dir, dist, k] = mouse.turnLeft()
+
+        finished = dir & 0b1000
+        left = dir & 0b0100
+        straight = dir & 0b0010
+        right = dir & 0b0001
+
+        location = updateLocation(curr.location, mouse, dist, LEFT)
         #acquire data from mouse and create new intersection
-        #set left (curr, intersect)
+        intersect = Intersection(left, straight, right, location, finished)
+        curr.setLeft(intersect)
+        
+        
+
+        mouse.updateNode(curr.left)
         check_children(curr.left)
+         
         if curr.finished == True:
             curr.prev.finished = True
             return 
+
     if curr.turns(2)==1 & curr.straight.dead == False:
-        #drive straight to next node
+        #turn_left and drive to next node(mouse_number)
+        [dir, dist, k] = mouse.turnStraight()
+
+        finished = dir & 0b1000
+        left = dir & 0b0100
+        straight = dir & 0b0010
+        right = dir & 0b0001
+
+        location = updateLocation(curr.location, mouse, dist, STRAIGHT)
+        #acquire data from mouse and create new intersection
+        intersect = Intersection(left, straight, right, location, finished)
+        curr.setStraight(intersect)
+        
+        mouse.updateNode(curr.straight)
         check_children(curr.straight)
+
         if curr.finished == True:
             curr.prev.finished = True
             return
+        
     if curr.turns(3)==1 & curr.right.dead == False:
-        #turn_right
-        #drive forward to next node
+        #turn_left and drive to next node(mouse_number)
+        [dir, dist, k] = mouse.turnRight()
+
+        finished = dir & 0b1000
+        left = dir & 0b0100
+        straight = dir & 0b0010
+        right = dir & 0b0001
+
+        location = updateLocation(curr.location, mouse, dist, RIGHT)
+        #acquire data from mouse and create new intersection
+        intersect = Intersection(left, straight, right, location, finished)
+        curr.setRight(intersect)
+        
+        mouse.updateNode(curr.right)
         check_children(curr.right)
+        
         if curr.finished == True:
             curr.prev.finished = True
             return
+
     set_node_dead(curr)
-    #U-Turn mouse and return to previous node
+    comm.send_instr(UTURN)
+    return_to_alive(curr, mouse)
 
 #Create First Intersection from mouse
-root  = getIntersection
-mouse1 = Mouse(1, root)
+# [dir, dist, k] = comm.send_instr(STRAIGHT)
+
+# finished = dir & 0b1000
+# left = dir & 0b0100
+# straight = dir & 0b0010
+# right = dir & 0b0001
+# location  = [0, 0]
+
+root = Intersection(0, 1, 0, [0,0], 0)
+mouse = Mouse(1, root)
+check_children(root, mouse)
 
 
 
